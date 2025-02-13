66d472425ebd104fa0c615698b46dcee41964820
rspartan-117 <riteksharmachd@gmail.com>
2025-02-13 15:00:38 +0530

    Changed icon Dimensions

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,14 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/icon.png',
-            sizes: '192x192',
-            type: 'image/png',
+            "src": "/icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
         ],
       },
