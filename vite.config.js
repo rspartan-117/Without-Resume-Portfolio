4288c986fd6111c27d796f7e11aec7c21d092f79
rspartan-117 <riteksharmachd@gmail.com>
2025-02-13 14:15:23 +0530

    Changed PWA icon

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,9 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/pwa-icon-192.webp',
+            src: '/icon.png',
             sizes: '192x192',
-            type: 'image/webp',
+            type: 'image/png',
           }
         ],
       },
